b600617278a9a0ea569203bf5c3f1e7f4fedc048
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-04-11 18:27:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,24 +24,25 @@ const logDriversByHometown = function (drivers, hometown) {
 };
 
 const driversByRevenue = function (drivers) {
-+  return  drivers.slice().sort(function (driver1,driver2){
-+    return driver1.revenue - driver2.revenue;
-+  });
-+};
-+
-+const driversByName = function (drivers) {
-+  return  drivers.slice().sort(function (driver1,driver2){
-+    return driver1.name.localeCompare(driver2.name);
-+  });
-+};
-+
-+const totalRevenue = function (drivers) {
-+  return drivers.reduce(function (totalRev, driver) {
-+    return totalRev + driver.revenue;
-+  }, 0);
-+};
-+
-+const averageRevenue= function (drivers) {
-+  return (totalRevenue(drivers))/drivers.length;
-+};
+  return  drivers.slice().sort(function (driver1,driver2){
+  return driver1.revenue - driver2.revenue;
+  });
+};
 
+const driversByName = function (drivers) {
+  return  drivers.slice().sort(function (driver1,driver2){
+    return driver1.name.localeCompare(driver2.name);
+  });
+};
+
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (totalRev, driver) {
+    return totalRev + driver.revenue;
+  }, 0);
+};
+
+case expression:
+
+  break;onst averageRevenue= function (drivers)
+  {return (totalRevenue(drivers))/drivers.length;
+};
