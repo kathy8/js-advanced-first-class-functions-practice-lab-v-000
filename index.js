8221c5e9f4c72fbfea430e7d00aab63cb153a893
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-04-11 18:24:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+//logs the name of each driver 
+//logDriverNames() — Receives an array of driver objects 
+//and logs the name attribute of each driver to the console.
+function logDriverNames(drivers){
+  drivers.forEach(calback);
+}
+
+const calback=function (el,i,array){
+  console.log(el.name);
+};
